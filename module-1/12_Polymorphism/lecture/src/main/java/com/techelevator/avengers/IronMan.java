4ccd1b24e8f45f94a3ba6cecb87961c3dbee3bce
Christopher Guarnera <christopher@techelevator.com>
2023-10-25 08:35:56 -0400

    lecture updates

---
+++ module-1/12_Polymorphism/lecture/src/main/java/com/techelevator/avengers/IronMan.java
@@ -0,0 +1,12 @@
+package com.techelevator.avengers;
+
+public class IronMan extends Avenger {
+
+    private int durability;
+
+    public IronMan(){
+        super("Iron Man", "Super suit, billionaire, philanthropist");
+
+    }
+
+}
