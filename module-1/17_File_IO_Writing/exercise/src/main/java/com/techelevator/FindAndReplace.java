d940dbcc9016d8059b23af997afd8a948cd8fe9f
Alex Baszuk <abaszuk@gmail.com>
2023-11-01 11:41:18 -0400

    file io p2 hw

--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class FindAndReplace {
@@ -14,7 +16,31 @@ public class FindAndReplace {
 
     public void run() {
         /* Your code goes here */
+        System.out.println("What is the search word?");
+        String searchWord = userInput.nextLine();
 
+        System.out.println("What is the replacement word?");
+        String replacementWord = userInput.nextLine();
+
+        System.out.println("What is the source file?");
+        String sourceFile = userInput.nextLine();
+
+        System.out.println("What is the destination file?");
+        String destinationFile = userInput.nextLine();
+
+        File source = new File(sourceFile);
+        File destination = new File(destinationFile);
+
+        try(Scanner reader = new Scanner(source); PrintWriter writer = new PrintWriter(destination)){
+
+            while(reader.hasNext()){
+                String line = reader.nextLine();
+                writer.println(line.replace(searchWord, replacementWord));
+            }
+
+        } catch (Exception ex){
+            System.out.println("Something went wrong");
+        }
 
     }
 
--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,5 +1,6 @@
 package com.techelevator;
 
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class FizzWriter {
@@ -14,6 +15,28 @@ public class FizzWriter {
 
 	public void run() {
 		/* Your code goes here */
+		System.out.println("What is the destination file?");
+		String destinationFile = userInput.nextLine();
+
+		try( PrintWriter writer = new PrintWriter(destinationFile)){
+
+			int lineCounter = 1;
+			while(lineCounter <= 300){
+				if (lineCounter % 3 == 0 && lineCounter % 5 ==0 ){
+					writer.println("FizzBuzz");
+				} else if (lineCounter % 3 == 0){
+					writer.println("Fizz");
+				} else if (lineCounter % 5 ==0){
+					writer.println("Buzz");
+				} else {
+					writer.println(lineCounter);
+				} lineCounter++;
+			}
+
+		} catch (Exception ex){
+			System.out.println("Something went wrong");
+		}
+
 
 
 	}
