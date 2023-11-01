c96c368acccddc3370b3403d7cb4eaa7830f52e5
Alex Baszuk <abaszuk@gmail.com>
2023-11-01 10:26:24 -0400

    file io p2 lecture

--- module-1/17_File_IO_Writing/lecture/src/main/java/com/techelevator/Lecture.java
+++ module-1/17_File_IO_Writing/lecture/src/main/java/com/techelevator/Lecture.java
@@ -20,6 +20,30 @@ public class Lecture {
 		 * A new instance of File can be created from a String that contains a file system path
 		 */
 
+		System.out.println("Please enter the fully qualified file name.");
+
+		String fileName = userInput.nextLine();
+
+		File file = new File(fileName);
+
+		//if the file does not exist create it
+		if (!file.exists()){
+			file.createNewFile();
+		} else {
+			System.out.println("How large is this file: " + file.length());
+			System.out.println("When was the file last modified: " + file.lastModified());
+			System.out.println("What is the absolute path of the file: " + file.getAbsolutePath());
+		}
+
+
+
+		System.out.println("Please enter the name of a new directory you want to create");
+		String directoryName = userInput.nextLine();
+		File directory = new File(directoryName);
+
+		if (!directory.exists()){
+			directory.mkdir();
+		}
 
 
 	}
