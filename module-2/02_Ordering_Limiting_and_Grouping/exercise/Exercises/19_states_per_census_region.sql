de05fecc64fe99492f4f6edd2251b9ac89c29eb2
Alex Baszuk <abaszuk@gmail.com>
2023-11-07 13:42:20 -0500

    hw

--- module-2/02_Ordering_Limiting_and_Grouping/exercise/Exercises/19_states_per_census_region.sql
+++ module-2/02_Ordering_Limiting_and_Grouping/exercise/Exercises/19_states_per_census_region.sql
@@ -2,4 +2,9 @@
 -- Order the results from highest to lowest.
 -- (Note: DC is in a census region, but the territories aren't, so the sum of the counts will equal 51).
 -- (4 rows)
+SELECT COUNT(state_name) as num_states, census_region
+FROM state
+WHERE census_region IS NOT null
+GROUP BY census_region
+ORDER BY num_states DESC
 
--- module-2/02_Ordering_Limiting_and_Grouping/exercise/Exercises/20_largest_city_per_state.sql
+++ module-2/02_Ordering_Limiting_and_Grouping/exercise/Exercises/20_largest_city_per_state.sql
@@ -1,4 +1,6 @@
 -- 20. The state abbreviation, and population of the city with the largest population (name column 'city_population') for all states, territories, and districts.
 -- Order the results from highest to lowest populations.
 -- (56 rows)
-
+SELECT state_abbreviation, population as city_population
+FROM state
+ORDER BY population DESC
