048379e7fbbe14f756a7cfdf3cfeb882a0407c36
Alex Baszuk <abaszuk@gmail.com>
2023-11-08 11:58:03 -0500

    lecture

--- module-2/02_Ordering_Limiting_and_Grouping/exercise/Exercises/20_largest_city_per_state.sql
+++ module-2/02_Ordering_Limiting_and_Grouping/exercise/Exercises/20_largest_city_per_state.sql
@@ -1,6 +1,7 @@
 -- 20. The state abbreviation, and population of the city with the largest population (name column 'city_population') for all states, territories, and districts.
 -- Order the results from highest to lowest populations.
 -- (56 rows)
-SELECT state_abbreviation, population as city_population
-FROM state
-ORDER BY population DESC
+SELECT state_abbreviation, MAX(population) as city_population
+FROM city
+GROUP BY state_abbreviation
+ORDER BY city_population DESC
--- module-2/03_Joins/lecture/lecture.sql
+++ module-2/03_Joins/lecture/lecture.sql
@@ -1,42 +1,78 @@
 -- INNER JOIN
 
 -- Write a query to retrieve the name and state abbreviation for the 2 cities named "Columbus" in the database
-
+SELECT city_name, state_abbreviation
+FROM city;
 
 -- Modify the previous query to retrieve the names of the states (rather than their abbreviations).
-
+SELECT city_name, city.state_abbreviation
+FROM city
+JOIN state ON city.state_abbreviation = state.state_abbreviation
+WHERE city_name = 'Columbus';
 
 -- Write a query to retrieve the names of all the national parks with their state abbreviations.
 -- (Some parks will appear more than once in the results, because they cross state boundaries.)
-
+SELECT park_name, state_abbreviation
+FROM park
+JOIN park_state on park.park_id = park_state.park_id;
 
 -- The park_state table is an associative table that can be used to connect the park and state tables.
 -- Modify the previous query to retrieve the names of the states rather than their abbreviations.
-
+SELECT park_name, state_name
+FROM park
+JOIN park_state on park.park_id = park_state.park_id
+JOIN state ON park_state.state_abbreviation = state.state_abbreviation;
 
 -- Modify the previous query to include the name of the state's capital city.
-
-
+SELECT park_name, state_name, city_name
+FROM park
+JOIN park_state on park.park_id = park_state.park_id
+JOIN state ON park_state.state_abbreviation = state.state_abbreviation
+JOIN city on state.capital = city.city_id;
 -- Modify the previous query to include the area of each park.
-
+SELECT park_name, state_name, city_name, park.area
+FROM park
+JOIN park_state on park.park_id = park_state.park_id
+JOIN state ON park_state.state_abbreviation = state.state_abbreviation
+JOIN city on state.capital = city.city_id;
 
 -- Write a query to retrieve the names and populations of all the cities in the Midwest census region.
-
+SELECT city_name, city.population
+FROM city
+JOIN state ON city.state_abbreviation = state.state_abbreviation
+WHERE census_region = 'Midwest';
 
 -- Write a query to retrieve the number of cities in the city table for each state in the Midwest census region.
-
+SELECT state.state_abbreviation, count(city_name)
+FROM city
+JOIN state ON city.state_abbreviation = state.state_abbreviation
+WHERE census_region = 'Midwest'
+GROUP BY state.state_abbreviation;
 
 -- Modify the previous query to sort the results by the number of cities in descending order.
-
+SELECT state.state_abbreviation, count(city_name) AS num_cities
+FROM city
+JOIN state ON city.state_abbreviation = state.state_abbreviation
+WHERE census_region = 'Midwest'
+GROUP BY state.state_abbreviation
+ORDER BY num_cities DESC;
 
 
 -- LEFT JOIN
 
 -- Write a query to retrieve the state name and the earliest date a park was established in that state (or territory) for every record in the state table that has park records associated with it.
-
+SELECT state_name, MIN(date_established)
+FROM state
+JOIN park_state ON state.state_abbreviation = park_state.state_abbreviation
+JOIN park ON park_state.park_id = park.park_id
+GROUP BY state.state_abbreviation;
 
 -- Modify the previous query so the results include entries for all the records in the state table, even if they have no park records associated with them.
-
+SELECT state_name, MIN(date_established)
+FROM state
+LEFT JOIN park_state ON state.state_abbreviation = park_state.state_abbreviation
+LEFT JOIN park ON park_state.park_id = park.park_id
+GROUP BY state.state_abbreviation;
 
 
 -- UNION
