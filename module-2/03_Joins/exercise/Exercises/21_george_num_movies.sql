e4f7d3a4dd40f0b9477186f837b0113a5b999ae9
Alex Baszuk <abaszuk@gmail.com>
2023-11-09 11:39:14 -0500

    lecture

--- module-2/03_Joins/exercise/Exercises/21_george_num_movies.sql
+++ module-2/03_Joins/exercise/Exercises/21_george_num_movies.sql
@@ -1,4 +1,10 @@
 -- 21. For every person in the person table with the first name of "George", list the number of movies they've been in. Name the count column 'num_of_movies'.
 -- Include all Georges, even those that have not appeared in any movies. Display the names in alphabetical order. 
 -- (59 rows)
-
+SELECT person_name, count(title) as num_of_movies
+FROM person
+LEFT JOIN movie_actor ON person.person_id = movie_actor.actor_id
+LEFT JOIN movie ON movie_actor.movie_id = movie.movie_id
+WHERE person_name LIKE 'George %'
+GROUP BY person_id
+ORDER BY person_name
--- module-2/04_INSERT_UPDATE_and_DELETE/lecture/lecture.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/lecture/lecture.sql
@@ -1,38 +1,87 @@
 -- INSERT
 
 -- Add Disneyland to the park table. (It was established on 7/17/1955, has an area of 2.1 square kilometers and does not offer camping.)
+INSERT INTO park(park_name, date_established, area, has_camping)
+VALUES('Disneyland', '7/17/1955', 2.1, false);
 
+SELECT *
+FROM park
+WHERE park_name = 'Disneyland';
 
 -- Add Hawkins, IN (with a population of 30,000 and an area of 38.1 square kilometers) and Cicely, AK (with a popuation of 839 and an area of 11.4 square kilometers) to the city table.
+INSERT INTO city(city_name, state_abbreviation, population, area)
+VALUES ('Hawkins', 'IN', 30000, 38.1), ('Cicely', 'AK', 839, 11.4);
 
+SELECT *
+FROM city
+WHERE city_name IN ('Hawkins', 'Cicely');
 
 -- Since Disneyland is in California (CA), add a record representing that to the park_state table.
-
+--the nubmer of columns returned from the select must match what we indicated in the insert into
+INSERT INTO park_state(park_id, state_abbreviation)
+--VALUES(64, 'CA')
+SELECT park_id, 'CA'
+FROM park
+WHERE park_name = 'Disneyland';
 
 
 -- UPDATE
 
 -- Change the state nickname of California to "The Happiest Place on Earth."
+select *
+FROM State
+WHERE state_abbreviation = 'CA'
 
+UPDATE state
+SET state_nickname = 'The Happiest Place on Earth'
+WHERE state_abbreviation = 'CA'
 
 -- Increase the population of California by 1,000,000.
-
+UPDATE state
+SET population = population + 1000000
+WHERE state_abbreviation = 'CA'
 
 -- Change the capital of California to Anaheim.
+SELECT city_id
+FROM city
+WHERE city_name = 'Anaheim'
 
+UPDATE state
+SET capital = (SELECT city_id
+				FROM city
+				WHERE city_name = 'Anaheim')
+WHERE state_abbreviation = 'CA'
 
 -- Change California's nickname back to "The Golden State", reduce the population by 1,000,000, and change the capital back to Sacramento.
-
+UPDATE state
+SET state_nickname='The Golden State',
+		capital = (SELECT city_id
+				FROM city
+				WHERE city_name = 'Sacramento'),
+				population = population - 1000000
+WHERE state_abbreviation = 'CA'
 
 
 -- DELETE
 
 -- Delete Hawkins, IN from the city table.
+-- the only difference from a delete and select is the delete and select
+SELECT *
+FROM city
+WHERE city_name = 'Hawkins'
 
+DELETE
+FROM city
+WHERE city_name = 'Hawkins'
 
 -- Delete all cities with a population of less than 1,000 people from the city table.
+SELECT * 
+FROM city
+WHERE population < 1000
 
-
+DELETE 
+FROM city
+WHERE population < 1000
 
 -- REFERENTIAL INTEGRITY
 
