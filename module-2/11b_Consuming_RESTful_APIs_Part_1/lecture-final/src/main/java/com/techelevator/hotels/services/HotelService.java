341bebb1ce7ca7c581c4984ea5cd0408d33be1d9
Christopher Guarnera <christopher@techelevator.com>
2023-11-20 11:52:48 -0500

    lecture & hw

---
+++ module-2/11b_Consuming_RESTful_APIs_Part_1/lecture-final/src/main/java/com/techelevator/hotels/services/HotelService.java
@@ -0,0 +1,52 @@
+package com.techelevator.hotels.services;
+
+import com.techelevator.hotels.model.City;
+import com.techelevator.hotels.model.Hotel;
+import com.techelevator.hotels.model.Review;
+import org.springframework.web.client.RestTemplate;
+
+public class HotelService {
+
+    private static final String API_BASE_URL = "http://localhost:3000";
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public Hotel[] listHotels() {
+
+        String url = API_BASE_URL + "/hotels";
+
+        Hotel[] hotels = restTemplate.getForObject(url, Hotel[].class );
+
+        return hotels;
+    }
+
+    public Review[] listReviews() {
+        String url = "http://localhost:3000/reviews";
+        Review[] reviews = restTemplate.getForObject(url, Review[].class);
+        return reviews;
+    }
+
+    public Hotel getHotelById(int id) {
+        String url = API_BASE_URL + "/hotels/" + id;
+        Hotel hotel = restTemplate.getForObject(url, Hotel.class);
+        return hotel;
+    }
+
+    public Review[] getReviewsByHotelId(int hotelId) {
+        //localhost:3000/hotels/1/reviews
+        String url = API_BASE_URL + "/hotels/" + hotelId + "/reviews";
+        Review[] reviews = restTemplate.getForObject(url, Review[].class);
+        return reviews;
+    }
+
+    public Hotel[] getHotelsByStarRating(int stars) {
+        //localhost:3000/hotels?stars=4
+        String url = API_BASE_URL + "/hotels?stars=" + stars;
+        Hotel[] hotels = restTemplate.getForObject(url, Hotel[].class);
+        return hotels;
+    }
+
+    public City getWithCustomQuery(){
+        return null;
+    }
+
+}
