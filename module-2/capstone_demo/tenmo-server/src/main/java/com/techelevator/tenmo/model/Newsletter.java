e280bb41bcbfcb0a1f9c0311dd6bb9a38a86b815
Christopher Guarnera <christopher@techelevator.com>
2023-11-28 16:45:33 -0500

    capstone demo

---
+++ module-2/capstone_demo/tenmo-server/src/main/java/com/techelevator/tenmo/model/Newsletter.java
@@ -0,0 +1,24 @@
+package com.techelevator.tenmo.model;
+
+public class Newsletter {
+
+    private int newsletterId;
+
+    private String newsletterName;
+
+    public int getNewsletterId() {
+        return newsletterId;
+    }
+
+    public void setNewsletterId(int newsletterId) {
+        this.newsletterId = newsletterId;
+    }
+
+    public String getNewsletterName() {
+        return newsletterName;
+    }
+
+    public void setNewsletterName(String newsletterName) {
+        this.newsletterName = newsletterName;
+    }
+}
