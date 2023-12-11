727ca422988cbce9f46cf6af7dbcdbe2c025f8c1
Christopher Guarnera <christopher@techelevator.com>
2023-12-11 11:52:43 -0500

    lecture and hw

---
+++ module-3/06_Intro_to_JavaScript/lecture-final/script.js
@@ -0,0 +1,184 @@
+
+console.log("Hello World");
+
+/*
+    Example of a multi-line comment just like in C#/Java
+*/
+
+// Single line comment
+
+/**
+ * Functions start with the word function.
+ * They don't have a return type and the naming convention is camel-case.
+ */
+function variables() {
+  // Declares a variable where the value cannot be changed
+  const myConst = 10;
+
+  //instead of System.out.println in java we can say console.log in javascript
+  console.log("My const variable is " + myConst);
+
+  // Declares a variable those value can be changed
+  let myVar = 10;
+  myVar = 100;
+  console.log("My variable is " + myVar);
+
+  // Declares a variable that will always be an array
+  const myArray = [1,2,3,4];
+  //myArray = 5;
+  myArray.push(5);
+  console.log("My array is " + myArray);
+}
+
+/**
+ * Functions can also accept parameters.
+ * Notice the parameters do not have types.
+ * @param {Number} param1 The first number to display
+ * @param {Number} param2 The second number to display
+ */
+function printParameters(param1, param2) {
+  console.log(`The value of param1 is ${param1}`);
+  console.log(`The value of param2 is ${param2}`);
+}
+
+/**
+ * Compares two values x and y.
+ * == is loose equality
+ * === is strict equality
+ * @param {Object} x
+ * @param {Object} y
+ */
+function equality(x, y) {
+  console.log(`x is ${typeof x}`);
+  console.log(`y is ${typeof y}`);
+
+  console.log(`x == y : ${x == y}`); // true
+  console.log(`x === y : ${x === y}`); // false
+}
+
+/**
+ * Each value is inherently truthy or falsy.
+ * false, 0, '', null, undefined, and NaN are always falsy
+ * everything else is always truthy
+ * @param {Object} x The object to check for truthy or falsy,
+ */
+function falsy(x) {
+  if (x) {
+    console.log(`${x} is truthy`);
+  } else {
+    console.log(`${x} is falsy`);
+  }
+}
+
+/**
+ *  Objects are simple key-value pairs
+    - values can be primitive data types
+    - values can be arrays
+    - or they can be functions
+*/
+function objects() {
+  const person = {
+    firstName: "Bill",
+    lastName: "Lumbergh",
+    age: 42,
+    employees: [
+      "Peter Gibbons",
+      "Milton Waddams",
+      "Samir Nagheenanajar",
+      "Michael Bolton"
+    ]
+  };
+
+  // Log the object
+  console.log(person);
+
+  // Log the first and last name
+  console.log(person.firstName + " " + person.lastName);
+
+  //We can dynamically add new fields
+  person.middleName = "Frank";
+  console.log(person);
+
+  // Log each employee
+  console.log(person.employees);
+
+  console.log("For Loop");
+  //regular for loop
+  for(let i = 0; i < person.employees.length; i++){
+    console.log( person.employees[i] );
+  }
+
+  console.log("Foreach loop")
+  //foreach loop
+  for(let name of person.employees){
+    console.log(name);
+  }
+
+}
+
+/*
+########################
+Function Overloading
+########################
+
+Function Overloading is not available in Javascript. If you declare a
+function with the same name, more than one time in a script file, the
+earlier ones are overriden and the most recent one will be used.
+*/
+
+function Add(num1, num2) {
+  return num1 + num2;
+}
+
+function Add(num1, num2, num3) {
+  return num1 + num2 + num3;
+}
+
+/*
+########################
+Math Library
+########################
+
+A built-in `Math` object has properties and methods for mathematical constants and functions.
+*/
+
+function mathFunctions() {
+  console.log("Math.PI : " + Math.PI);
+  console.log("Math.LOG10E : " + Math.LOG10E);
+  console.log("Math.abs(-10) : " + Math.abs(-10));
+  console.log("Math.floor(1.99) : " + Math.floor(1.99));
+  console.log("Math.ceil(1.01) : " + Math.ceil(1.01));
+  console.log("Math.random() : " + Math.random());
+
+  //if we want to parse an int from a string
+  let myInt = parseInt("50");
+  console.log(myInt);
+
+  let myFloat = parseFloat("2.3");
+  console.log(myFloat);
+}
+
+/*
+########################
+String Methods
+########################
+
+The string data type has a lot of properties and methods similar to strings in Java/C#
+*/
+
+function stringFunctions(value) {
+  console.log(`.length -  ${value.length}`);
+  console.log(`.endsWith('World') - ${value.endsWith("World")}`);
+  console.log(`.startsWith('Hello') - ${value.startsWith("Hello")}`);
+  console.log(`.indexOf('Hello') - ${value.indexOf("Hello")}`);
+
+  /*
+    Other Methods
+        - split(string)
+        - substr(number, number)
+        - substring(number, number)
+        - toLowerCase()
+        - trim()
+        - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+    */
+}
